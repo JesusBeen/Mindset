58fff7d74f29ff7fda84d6b54a1c9000abfc9260
JesusBeen <benjas702@hotmail.com>
2015-10-19 18:26:17 -0500

    new1

--- app/src/main/java/com/jbjq/benjamin/mindset/Palabras_1.java
+++ app/src/main/java/com/jbjq/benjamin/mindset/Palabras_1.java
@@ -1,22 +1,69 @@
 package com.jbjq.benjamin.mindset;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.Paint;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.SystemClock;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.TextView;
+import java.util.concurrent.TimeUnit;
 
 public class Palabras_1 extends Activity {
+
+    Button iniciar,palabra1,palabra2,palabra3,palabra4;
+    TextView texto1,texto2,texto3;
+    Chronometer crono;
+    long Time =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palabras_1);
 
+        crono = (Chronometer)findViewById(R.id.chronometer);
+        iniciar = (Button)findViewById(R.id.button23);
+
+
+        palabra1 = (Button)findViewById(R.id.button13);
+        palabra2 = (Button)findViewById(R.id.button14);
+        palabra3 = (Button)findViewById(R.id.button15);
+        palabra4 = (Button)findViewById(R.id.button16);
+
+        texto1 = (TextView)findViewById(R.id.textView);
+        texto2 = (TextView)findViewById(R.id.textView2);
+        texto3 = (TextView)findViewById(R.id.textView3);
+
+        iniciar.setEnabled(true);
+        iniciar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                crono.setBase(SystemClock.elapsedRealtime());
+                crono.start();
+                crono.setVisibility(View.VISIBLE);
+                iniciar.setVisibility(View.INVISIBLE);
+
+                texto1.setVisibility(View.VISIBLE);
+                texto2.setVisibility(View.VISIBLE);
+                texto3.setVisibility(View.VISIBLE);
+
+                palabra1.setVisibility(View.VISIBLE);
+                palabra2.setVisibility(View.VISIBLE);
+                palabra3.setVisibility(View.VISIBLE);
+                palabra4.setVisibility(View.VISIBLE);
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
